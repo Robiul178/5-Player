30358b2080f0641a27bbd88d30349a1e9a61be06
Robiul Islam <robiulislam.me2022@gmail.com>
2022-08-23 04:12:32 +0600

    sending player

---
+++ selece-five-player/for-player-select.js
@@ -0,0 +1,34 @@
+
+  //For Martinez//
+document.getElementById('messi').addEventListener("click", function(){
+    //kar moddhe add korbo
+    const listContainer = document.getElementById('list-container');
+    //list item create korte hbe
+    const li = document.createElement('li');
+    li.innerText='Lionel Messi'; //click korar por remove na howar karon class add kora hoynai
+    li.classList.add('listItem')
+    listContainer.appendChild(li);
+  }) 
+  //For Martinez//
+document.getElementById('martinez').addEventListener("click", function(){
+    //kar moddhe add korbo
+    const listContainer = document.getElementById('list-container');
+    //list item create korte hbe
+    const li = document.createElement('li');
+    li.innerText='Emiliaon Martinez'; //click korar por remove na howar karon class add kora hoynai
+    li.classList.add('listItem')
+    listContainer.appendChild(li);
+  }) 
+  
+
+  //For Sadio//
+document.getElementById('sadio').addEventListener("click", function(){
+    //kar moddhe add korbo
+    const listContainer = document.getElementById('list-container');
+    //list item create korte hbe
+    const li = document.createElement('li');
+    li.innerText='Sadio Mane'; //click korar por remove na howar karon class add kora hoynai
+    li.classList.add('listItem')
+    listContainer.appendChild(li);
+  }) 
+  
