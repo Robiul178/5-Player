8834405fbb117ebf2d525253d1773674e31d9209
Robiul Islam <robiulislam.me2022@gmail.com>
2022-08-23 06:02:08 +0600

    budget section sending

--- selece-five-player/for-player-select.js
+++ selece-five-player/for-player-select.js
@@ -1,21 +1,17 @@
 
-  //For Martinez//
+  //For Messi//
 document.getElementById('messi').addEventListener("click", function(){
-    //kar moddhe add korbo
     const listContainer = document.getElementById('list-container');
-    //list item create korte hbe
     const li = document.createElement('li');
-    li.innerText='Lionel Messi'; //click korar por remove na howar karon class add kora hoynai
+    li.innerText='Lionel Messi';
     li.classList.add('listItem')
     listContainer.appendChild(li);
   }) 
   //For Martinez//
 document.getElementById('martinez').addEventListener("click", function(){
-    //kar moddhe add korbo
     const listContainer = document.getElementById('list-container');
-    //list item create korte hbe
     const li = document.createElement('li');
-    li.innerText='Emiliaon Martinez'; //click korar por remove na howar karon class add kora hoynai
+    li.innerText='Emiliaon Martinez';
     li.classList.add('listItem')
     listContainer.appendChild(li);
   }) 
@@ -23,11 +19,37 @@ document.getElementById('martinez').addEventListener("click", function(){
 
   //For Sadio//
 document.getElementById('sadio').addEventListener("click", function(){
-    //kar moddhe add korbo
     const listContainer = document.getElementById('list-container');
-    //list item create korte hbe
     const li = document.createElement('li');
-    li.innerText='Sadio Mane'; //click korar por remove na howar karon class add kora hoynai
+    li.innerText='Sadio Mane'; 
+    li.classList.add('listItem')
+    listContainer.appendChild(li);
+  }) 
+  
+
+  //For Ronaldo//
+document.getElementById('ronaldo').addEventListener("click", function(){
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement('li');
+    li.innerText='Crisiano Ronaldo';
+    li.classList.add('listItem')
+    listContainer.appendChild(li);
+  }) 
+  //For Maradona//
+document.getElementById('maradona').addEventListener("click", function(){
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement('li');
+    li.innerText='Diago Maradona';
+    li.classList.add('listItem')
+    listContainer.appendChild(li);
+  }) 
+  
+
+  //For marcelo//
+document.getElementById('marcelo').addEventListener("click", function(){
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement('li');
+    li.innerText='Marcelo Vieira'; 
     li.classList.add('listItem')
     listContainer.appendChild(li);
   }) 
